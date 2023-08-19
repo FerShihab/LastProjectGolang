1b721b2884c2d35c35a78205e4eed7ea4d0ac02b
FShihab <fergiant.shihab.barrans.id>
2023-08-19 21:07:35 +0700

    first and last commit3

--- configs/config.go
+++ configs/config.go
@@ -1,7 +1,9 @@
 package configs
 
 import (
+	"fmt"
 	"lastProject/models"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,7 +12,12 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
-	dsn := "root:Clayjr54!@tcp(127.0.0.1:3306)/prakerja8?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_ROOT"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
